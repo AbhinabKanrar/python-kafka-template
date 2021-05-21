f74725a02816b57f56a11836f0857e125085772a
Abhinab Kanrar <abhinabkanrar@gmail.com>
2021-05-21 06:19:22 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,3 @@
+from src.service.consumer_service import ConsumerService
+
+ConsumerService().load_consumer()
---
+++ format.sh
@@ -0,0 +1,5 @@
+flake8 src/
+
+black src/
+
+autopep8 --in-place --aggressive --recursive src
